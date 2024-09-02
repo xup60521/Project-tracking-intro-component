d9e5c56e2fea01b3bbb3a293c8433b5cb9c0b07d
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-09-02 23:15:55 +0800

    finish desktop layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Red: "hsl(0, 100%, 68%)",
+                c_Very_Dark_Blue: "hsl(230, 29%, 20%)",
+                c_Dark_Grayish_Blue: "hsl(230, 11%, 40%)",
+                c_Grayish_Blue: "hsl(231, 7%, 65%)",
+                c_Light_Grayish_Blue: "hsl(207, 33%, 95%)",
             },
             fontFamily: {
-
+                body: ["Barlow", "system-ui"],
+                other: ["Barlow Condensed", "sans-serif"]
             }
         },
     },
